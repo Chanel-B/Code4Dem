3b76726355c15ce67609fea0ab6da5f477a9b495
Chanel-B <84045976+Chanel-B@users.noreply.github.com>
2022-04-03 10:41:43 +0200

    First commit added spring project with web dependency

---
+++ myApp/src/test/java/com/code4dem/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4dem.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
